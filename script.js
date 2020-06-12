827ea99f4b5ace27dc0958815b13662b78be58ac
slavyanvv <uglyman1989@gmail.com>
2020-06-12 13:09:27 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,133 @@
+//alert(1)
+//[0.3].push()
+
+
+
+//var leftBorderWidth = 1;
+//{
+//    let second1 = 2;
+//}
+//let second1 = 2;
+//const pi = 3.14;
+//
+//console.log(second1);
+
+
+
+//var number = 5;
+//var string = "Hello!";
+//var sym = Symbol();
+//var boolean = true;
+//null;
+//undefined;
+//var obj = {};
+//
+//console.log(4/0);
+//console.log('string'*9);
+
+
+//let som;
+//console.log(som);
+
+//let persone = {
+//    name: "John",
+//    age: 25,
+//    isMarried: false
+//}
+//
+//console.log(persone["age"]);
+//
+//let arr = ['age','pythin','cobra'];
+//
+//console.log(arr[2])
+
+
+// - to String
+
+
+//1)  
+
+//console.log(typeof(String(4)));
+
+
+//2)
+
+//console.log(typeof("ww"+"51"));
+
+
+//-to Numbber
+
+//console.log(typeof(Number('51')));
+
+
+//console.log(typeof(5 + +'5'));
+
+//console.log(typeof(parseInt('15px',10)));
+
+
+//let ans = +prompt("Hello?", '');
+
+//0, '', null, undefined, NaN,
+
+//let switcher = null;
+
+//let switcher = null;
+
+//if (switcher) {
+  //  console.log("Working..")
+//}
+
+//switcher = 1;
+
+//if (switcher) {
+//    console.log("Working..")
+//}
+
+//2 
+//console.log(typeof(Boolean('5')));
+//3 
+//console.log(typeof(!!'5'));
+
+
+
+//let arr = ['age','pythin','cobra'];
+
+//console.log(arr[2])
+
+
+//alert("Hello world!");
+
+//let answer = +prompt("Are you there 18?", "Pizda");
+
+//console.log(typeof(answer))
+
+//console.log(4 + + "- object");
+
+
+let incr = 10,
+    decr = 10;
+
+  //  incr++;
+//    decr--;
+
+    console.log(incr--);
+    console.log(decr++);
+
+    console.log(5%2);
+    console.log("2" == 2);
+
+let isChecked = false;
+    isClosed = false;
+
+    console.log(isChecked || !isClosed)
+
+
+
+
+
+
+
+
+
+
+
